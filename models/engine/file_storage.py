78fe5dacf5cb4373f1045e8512d35b2ef6b293f0
laila22haz <hazmirlaila316@gmail.com>
2023-08-10 20:12:23 +0100

    create base_model and file_storage straggling a little with reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Build storage class"""
-from models.base_model import BaseModel
 import json
 import os
+from models.base_model import BaseModel
+"""Build storage class"""
+
 
 class FileStorage():
     """Class that serializes instances to a JSON file and deserializes JSON file to instances"""
@@ -17,21 +18,22 @@ class FileStorage():
         self.__objects[key] = obj
 
     def save(self):
-        new_dict = {} #First, when an instance of MyBase is created, it will be passed 
+        new_dict = {}  # First, when an instance of MyBase is created, it will be passed
         for key, value in self.__objects.items():
-            new_dict[key] = value.to_dict() #into save_to_dict() method that converts this instance or object to a Python dictionary representation.
-        with open(self.__file_path, "w", encoding="utf-8") as file:
-            json.dump(new_dict, file, indent=4)#The dictionary representation will then be passed into the Python JSON module, which uses the Json.dump() function to convert the dictionary representation to a JSON string, which is subsequently written into a file.
-
+            # into save_to_dict() method that converts this instance or object to a Python dictionary representation.
+            new_dict[key] = value.to_dict()
+        with open(self.__file_path, "w") as file:
+            # The dictionary representation will then be passed into the Python JSON module, which uses the Json.dump() function to convert the dictionary representation to a JSON string, which is subsequently written into a file.
+            json.dump(new_dict, file, indent=4)
 
     def reload(self):
         try:
-                with open(self.__file_path, "r", encoding="utf-8") as file:
-                    data = json.load(file)
+            with open(self.__file_path, "r") as file:
+                data = json.load(file)
                 for key, value in data.items():
                     name = value["__class__"]
                     name_exist = globals().get(name)
-                if name_exist:
-                    self.new(name_exist(**value))
+                    if name_exist:
+                        self.new(name_exist(**value))
         except FileNotFoundError:
             pass

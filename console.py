8504107cfc9c3693c5c28f87227c76516f0f749f
Halmouus <javierminimaz@gmail.com>
2023-08-12 17:51:27 +0100

    Checking Pycodestyle and adding docstrings (still ongoing)

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Console for Airbnb project"""
 import cmd
 import re
 from models.base_model import BaseModel
@@ -10,28 +11,35 @@ from models.place import Place
 from models.review import Review
 from models import storage
 
-    
+
 def check_class(name, cl_name):
-    '''checks if name is of class cl_name or one of its subclasses'''
+    """checks if name is of class cl_name or one of its subclasses"""
     return name in globals() and \
         (name == cl_name or
-         issubclass(globals()[name],globals()[cl_name]))
+         issubclass(globals()[name], globals()[cl_name]))
+
 
 def cmd_parsing(expr):
+    """Parser function"""
     pattern = r"(\w+)\.(\w+)(\((.*?)\))?"
     match = re.match(pattern, expr)
     if match:
         groups = match.groups()
-        c_name, command, args = groups[0], groups[1], groups[3].split(',') if groups[3] else []
-        return [command, c_name] + [el.strip('" ') for el in args if len(args) > 0]
+        c_name, command, args = (groups[0], groups[1],
+                                 groups[3].split(',') if groups[3] else [])
+        return ([command, c_name] +
+                [el.strip('" ') for el in args if len(args) > 0])
     return []
 
+
 class HBNBCommand(cmd.Cmd):
+    """class that inherits from the cmd.Cmd class"""
     prompt = "(hbnb) "
     ruler = "="
     doc_header = "Documented commands (type help <topic>):"
 
     def default(self, line):
+        """triggered if command is undefined"""
         if line:
             cmd_lst = cmd_parsing(line)
             if len(cmd_lst) < 2:
@@ -46,7 +54,7 @@ class HBNBCommand(cmd.Cmd):
                     eval(f"self.do_{command}('{cl_name} {strs}')")
 
     def do_create(self, line):
-        '''Creates a new instance of a defined class'''
+        """Creates a new instance of a defined class"""
         if not line:
             print("** class name missing **")
         else:
@@ -57,14 +65,14 @@ class HBNBCommand(cmd.Cmd):
                 inst = eval(f"{args[0]}()")
                 inst.save()
                 print(inst.id)
-             
+
     def help_create(self):
-        '''displays the corresponding help message'''
+        """displays the corresponding help message"""
         print("Creates a new instance of a defined class")
 
     def do_show(self, line):
-        '''Prints the string representation of an instance\
-        based on the class name and id'''
+        """Prints the string representation of an instance\
+        based on the class name and id"""
         if not line:
             print("** class name missing **")
         else:
@@ -82,13 +90,13 @@ class HBNBCommand(cmd.Cmd):
                 print(obj)
 
     def help_show(self):
-        '''displays the corresponding help message'''
+        """displays the corresponding help message"""
         print('\n'.join([' '.join(["Prints the string representation of an",
-                        "instance based on the class name and id"]),
-                        "Usage : show <class_name> <instance id>"]))
-    
+                         "instance based on the class name and id"]),
+                         "Usage : show <class_name> <instance id>"]))
+
     def do_destroy(self, line):
-        '''Deletes an instance based on the class name and id'''
+        """Deletes an instance based on the class name and id"""
         if not line:
             print("** class name missing **")
         else:
@@ -107,12 +115,12 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
 
     def help_destroy(self):
-        '''displays the corresponding help message'''
+        """displays the corresponding help message"""
         print("Deletes an instance based on the class name and id")
 
     def do_all(self, arg):
-        '''Prints all string representation of all instances
-        based or not on the class name'''
+        """Prints all string representation of all instances
+        based or not on the class name"""
         dict = storage.all()
         if not arg:
             lst = [str(dict[obj]) for obj in dict.keys()]
@@ -131,13 +139,13 @@ class HBNBCommand(cmd.Cmd):
                 print(lst)
 
     def help_all(self):
-        '''displays the corresponding help message'''
+        """displays the corresponding help message"""
         print(' '.join(["Prints all string representation of all",
-                      "instances based or not on the class name"]))
-    
+                        "instances based or not on the class name"]))
+
     def do_update(self, line):
-        '''Updates an instance based on the class name and id
-        by adding or updating attribute'''
+        """Updates an instance based on the class name and id
+        by adding or updating attribute"""
         if not line:
             print("** class name missing **")
         else:
@@ -159,17 +167,16 @@ class HBNBCommand(cmd.Cmd):
                 obj = dict[key]
                 setattr(obj, attr, val)
                 storage.save()
-                
-    
-        
+
     def help_update(self):
+        """Displays help message for update cmd"""
         print('\n'.join([' '.join(["Updates an instance based on the class",
-                        "name and id by adding or updating attribute"]),
-                        ' '.join(['Usage: update <class name> <id>',
-                                  '<attribute name> "<attribute value>"'])]))
+                         "name and id by adding or updating attribute"]),
+                         ' '.join(['Usage: update <class name> <id>',
+                                   '<attribute name> "<attribute value>"'])]))
 
     def do_count(self, line):
-        '''Creates a new instance of a defined class'''
+        """Creates a new instance of a defined class"""
         count = 0
         if not line:
             print("** class name missing **")
@@ -185,15 +192,15 @@ class HBNBCommand(cmd.Cmd):
                     if (cl_name == args[0]):
                         count += 1
                 print(count)
-                
 
     def do_quit(self, line):
-        '''Quit command to exit the program'''
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, line):
-        '''EOF command to exit the program'''
+        """EOF command to exit the program"""
         return True
 
+
 if __name__ == '__main__':
- HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()

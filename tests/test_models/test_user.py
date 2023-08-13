064d69f3b09b3f24c78c7d2c4e0fd99466e8c3b5
laila22haz <hazmirlaila316@gmail.com>
2023-08-13 18:24:04 +0100

    unittest test_user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,4 +1,46 @@
 #!/usr/bin/python3
-"""Unittest for
+"""
+Unittest for User Class
 """
 import unittest
+from models.user import User
+from models.base_model import BaseModel
+import os
+
+
+class TestUser(unittest.TestCase):
+    """Test User class"""
+    def setUp(self):
+        self.user = User()
+
+    def tearDown(self):
+        """method that delete the user"""
+        del self.user
+
+    def test_empty(self):
+        """method that test if the attributes are empty"""
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
+
+        self.assertTrue(issubclass(User, BaseModel))
+
+    def test_assignment(self):
+        """method that assign the attributes"""
+        email_value = "test@gmail.com"
+        self.user.email = email_value
+        password_value = "azertyuiop"
+        self.user.password = password_value
+        first_name_val = "SponjBoB"
+        self.user.first_name = first_name_val
+        last_name_value = "AirBnB"
+        self.user.last_name = last_name_value
+
+        self.assertEqual(self.user.email, email_value)
+        self.assertEqual(self.user.password, password_value)
+        self.assertEqual(self.user.first_name, first_name_val)
+        self.assertEqual(self.user.last_name, last_name_value)
+
+if __name__ == '__main__':
+    unittest.main()

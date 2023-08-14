f9b3de57d6ea100e4ad5a03f1ecab939bdf092a2
laila22haz <hazmirlaila316@gmail.com>
2023-08-14 01:41:43 +0100

    Fix the pycodestyle for basemodel file v6

--- models/base_model.py
+++ models/base_model.py
@@ -47,4 +47,3 @@ class BaseModel():
     def __str__(self):
         """string representation of a BaseModel instance"""
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-
